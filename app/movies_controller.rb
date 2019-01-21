18fde992ae09447ad451c0efc751b0d3f6610f12
Marcus  = Soulo <souloinc87@gmail.com>
2019-01-21 20:25:14 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -72,7 +72,8 @@ def can_be_found_updated_and_saved
   movie = Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
-  move.save
+  movie.save
+  movie 
 end
 
 def can_update_using_update_method
